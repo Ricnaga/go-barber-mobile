2d6f09d351064bc171f33243216b1bda1c2c2450
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-11-13 16:54:36 -0300

    still doing input

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,25 @@
+import {Image} from 'react-native'
 import React from 'react';
-import {Container} from './styles'
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import logoImg from '../../assets/logo.png';
+import {Container, Title} from './styles'
 
 const SignIn: React.FC = () => {
     return(
-        <Container/>
+        <Container>
+            <Image source={logoImg}/>
+            
+            <Title>Faça seu logon</Title>
+            
+            <Input />
+            
+            <Input />
+
+            <Button onPress={() => {}}>Entrar</Button>
+        </Container>
     )
 }
 
