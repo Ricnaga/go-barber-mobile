58de28b9425778279f83c45a408037f343324d14
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-12-07 12:08:41 -0300

    create datepicker done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import 'react-native-gesture-handler'
+import 'react-native-gesture-handler';
 
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {View, StatusBar} from 'react-native';
-import Routes from './routes'
-import AppProvider from './hooks'
+import { NavigationContainer } from '@react-navigation/native';
+import { View, StatusBar } from 'react-native';
+import Routes from './routes';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
-    <NavigationContainer>
-        <StatusBar barStyle="light-content" backgroundColor='#312E38'/>
-        <AppProvider>
-            <View style={{flex:1, backgroundColor: '#312E38'}}>
-                <Routes />
-            </View>
-        </AppProvider>
-    </NavigationContainer>
-)
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312E38" translucent />
+    <AppProvider>
+      <View style={{ flex: 1, backgroundColor: '#312E38' }}>
+        <Routes />
+      </View>
+    </AppProvider>
+  </NavigationContainer>
+);
 
 export default App;
